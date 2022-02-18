003d0b14c2cc45f42439d0dcf960aec7992b4d07
Hugh Nowlan <hnowlan@wikimedia.org>
2022-02-18 12:33:19 +0000

    Switch to gookit color module, use lolcat colour output
    Get better rainbow output using the lolcat rgb method, switch
    libraries for more fine RBG control.

--- raingow.go
+++ raingow.go
@@ -3,40 +3,30 @@ package main
 
 import (
 	"fmt"
-	"github.com/fatih/color"
+	"math"
+	"github.com/gookit/color"
 )
 
-func Raingow_line(source_str string, offset int, reverse_palette bool, span int) string {
+// Wholesale borrowed from https://github.com/busyloop/lolcat
+func colour_code(freq float64, spread float64) color.RGBColor {
+	red   := uint8(math.Sin(freq * spread + 0) * 127 + 128)
+	green := uint8(math.Sin(freq * spread + 2* math.Pi/3) * 127 + 128)
+	blue  := uint8(math.Sin(freq * spread + 4* math.Pi/3) * 127 + 128)
+	rgb_obj := color.RGB(red, green, blue)
+	return rgb_obj
+}
 
-	//var colours = [7]*color.Color{
-	var colours = [6]*color.Color{
-		color.New(color.FgYellow),
-		color.New(color.FgRed),
-		color.New(color.FgGreen),
-		color.New(color.FgBlue),
-		color.New(color.FgMagenta),
-		color.New(color.FgCyan),
-		//color.New(color.FgWhite),
-	}
 
-	var output_str = ""
+func Raingow_line(source_str string, offset int, reverse_palette bool, span float64) string {
+	// TODO give the ability to pass state of colour_index between
+	// iterations (or a different wrapper function) so that we can
+	// span colours across multiple lines
 
-	var span_count = 0
-	var colour_index = 0
+	output_str := ""
 
-	for i := 0; i < len(source_str); i++ {
-		current_char := string(source_str[i])
-
-		if span_count < span {
-			span_count += 1
-		} else {
-			colour_index = (i + offset) % len(colours)
-			if reverse_palette {
-				colour_index = ( len(colours) - 1) - ((i + offset) % len(colours))
-			}
-			span_count = 0
-		}
-		output_str = output_str + colours[colour_index].SprintFunc()(current_char)
+	for j := 0; j < len(source_str); j++ {
+		the_colour := colour_code(0.1, float64(j) * span)
+		output_str += the_colour.Sprintf(string(source_str[j]))
 	}
 
 	return output_str
@@ -44,18 +34,20 @@ func Raingow_line(source_str string, offset int, reverse_palette bool, span int)
 
 
 func main() {
+
+
 	// TODO why does span lead to the same colour after a few repetitions when the spam is len(colours)?
-	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line", 0, false, 0))
-	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line", 0, false, 2))
-	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line", 0, false, 3))
-	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line", 0, false, 4))
-	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line", 0, false, 5))
-	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line", 0, false, 6))
-	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line", 0, false, 7))
-	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line", 0, false, 8))
-	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line", 0, false, 9))
-	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line reversed", 0, true, 0))
-	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line offset 1", 1, false, 0))
-	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line offset 2", 2, false, 0))
-	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line offset 3", 3, false, 0))
+	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line", 0, false, 1.0))
+	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line", 0, false, 2.0))
+	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line", 0, false, 3.0))
+	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line", 0, false, 4.0))
+	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line", 0, false, 5.0))
+	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line", 0, false, 6.0))
+	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line", 0, false, 7.0))
+	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line", 0, false, 8.0))
+	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line", 0, false, 9.0))
+	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line reversed", 0, true, 3.0))
+	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line offset 1", 1, false, 3.0))
+	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line offset 2", 2, false, 3.0))
+	fmt.Println(Raingow_line("My fairly long long long long long long long long long testing line offset 3", 3, false, 3.0))
 }
